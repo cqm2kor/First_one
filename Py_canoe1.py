39a305065a114d7a687a4f803a445cdc5d6e9c11
cqm2kor <charan.kumarts@in.bosch.com>
2025-02-10 17:08:30 +0530

    Add files via upload

---
+++ Py_canoe1.py
@@ -0,0 +1,17 @@
+
+from py_canoe import CANoe
+from time import sleep as wait
+
+canoe_inst = CANoe()
+canoe_inst.open(canoe_cfg=r"c:\Users\Public\Documents\Vector\CANoe\Sample Configurations 18.3.118\CAN\CANSystemDemo\CANSystemDemo.cfg")
+
+canoe_inst.start_measurement()
+wait(2)
+
+#sys_var_val = canoe_inst.get_system_variable_value('ComfortBus::ShowMultiDisplay')
+#print(sys_var_val)
+
+#resp = canoe_inst.send_diag_request('Door_Left', '1A 90')
+#print(resp)
+canoe_inst.stop_measurement()
+
