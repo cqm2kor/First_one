fd10537b7fd6bcd33de56201d8f3b53305a5d62f
cqm2kor <charan.kumarts@in.bosch.com>
2025-02-10 17:09:19 +0530

    Update Py_canoe1.py

--- Py_canoe1.py
+++ Py_canoe1.py
@@ -10,7 +10,7 @@ wait(2)
 
 #sys_var_val = canoe_inst.get_system_variable_value('ComfortBus::ShowMultiDisplay')
 #print(sys_var_val)
-
+print("Hello World")
 #resp = canoe_inst.send_diag_request('Door_Left', '1A 90')
 #print(resp)
 canoe_inst.stop_measurement()
